bd5e12b501101edb0f225265673d648d020b2386
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2019-04-04 19:12:06 +0200

    Revert "don't handle WM_SETCURSOR in fullscreen or windowedfullscreen modes"
    This reverts commit c3c1d41f2232be4d84b0fe59893a68ce3c638d04.

--- src/ddraw.cpp
+++ src/ddraw.cpp
@@ -425,9 +425,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         }
         case WM_SETCURSOR:
         {
-            if (Fullscreen || WindowedFullscreen)
-                break;
-
             // show resize cursor on window borders
             if ((HWND)wParam == hwnd_main)
             {
